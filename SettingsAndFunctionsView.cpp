67d327272584e73017928fe8ea0e458bfe46fcad
Martin Baar <martin.baar@gmail.com>
2014-12-18 15:06:08 +0100

    SEKM-18 Finish Quantization setting changes (forgotten submit)

--- SettingsAndFunctionsView.cpp
+++ SettingsAndFunctionsView.cpp
@@ -12,13 +12,14 @@ SettingsAndFunctionsView::SettingsAndFunctionsView() : hw_(0),
 								 settings_(0),
 								 instrumentBar_(0),
 								 buttonMap_(0),
-								 panButtons_(0) {
+								 quantizationButtons_(0) {
 }
 
 SettingsAndFunctionsView::~SettingsAndFunctionsView() {
-	//delete panButtons_;
+	delete quantizationButtons_;
 	instrumentBar_->setActive(true);
 }
+
 
 void SettingsAndFunctionsView::init(ILEDsAndButtonsHW * hw, PlayerSettings * settings,
 						 	 	 	 	InstrumentBar * instrumentBar, IButtonMap * buttonMap) {
@@ -27,21 +28,14 @@ void SettingsAndFunctionsView::init(ILEDsAndButtonsHW * hw, PlayerSettings * set
 	instrumentBar_ = instrumentBar;
 	instrumentBar_->setActive(false);
 	buttonMap_ = buttonMap;
-	//panButtons_ = new RadioButtons(hw_, buttonMap_->getSubStepButtonArray(), 4);
+	quantizationButtons_ = new RadioButtons(hw_, buttonMap_->getSubStepButtonArray(), 3);
 	instrumentButtons_.init(hw_, buttonMap_->getInstrumentButtonArray(), 6);
-
-	//Update pan buttons so it selects pan where the selected pattern is
-	/*for (unsigned char i = 0; i < 4; i++) {
-		bool inCurrentPan = (i == (currentPattern_ / 16));
-		hw_->setLED(buttonMap_->getSubStepButtonIndex(i), inCurrentPan ? ILEDHW::ON : ILEDHW::OFF);
-		if (inCurrentPan) {
-			panButtons_->setSelectedButton(i);
-		}
+	char buttonIndex = ((char)(settings_->getRecordQuantizationType())) - 1;
+	if (buttonIndex != -1) {
+		quantizationButtons_->setSelectedButton(buttonIndex);
 	}
 
-	updatePan(false);
-
-	*/
+	reflectQuantizationSettings();
 
 	for (unsigned char i = 0; i < 6; i++) {
 		bool isOn = settings_->getDrumInstrumentEventType(i) == PlayerSettings::GATE;
@@ -51,23 +45,28 @@ void SettingsAndFunctionsView::init(ILEDsAndButtonsHW * hw, PlayerSettings * set
 
 }
 
-void SettingsAndFunctionsView::updatePan(bool readHWStatus) {
-	/*if (readHWStatus) {
-		panButtons_->update();
+void SettingsAndFunctionsView::reflectQuantizationSettings() {
+	unsigned char index = (char)(settings_->getRecordQuantizationType());
+	for (unsigned char buttonIndex = 0; buttonIndex < 3; buttonIndex++) {
+		hw_->setLED(buttonMap_->getSubStepButtonIndex(buttonIndex),
+				buttonIndex < index ? ILEDHW::ON : ILEDHW::OFF);
 	}
-	unsigned char selectedPan;
-	panButtons_->getSelectedButton(selectedPan);
-	bool inCurrentPan = (selectedPan == (currentPattern_ / 16));
-	for (unsigned char i = 0; i < 16; i++) {
-		bool currentButton = (i == (currentPattern_ % 16));
-		hw_->setLED(buttonMap_->getStepButtonIndex(i), inCurrentPan && currentButton ? ILEDHW::ON : ILEDHW::OFF);
-	}*/
 }
+
 
 void SettingsAndFunctionsView::update() {
 
-	updatePan();
 	instrumentButtons_.update();
+	quantizationButtons_->update();
+
+	unsigned char quantizationIndex = 0;
+	PlayerSettings::QuantizationType newType = quantizationButtons_->getSelectedButton(quantizationIndex) ?
+			(PlayerSettings::QuantizationType)(quantizationIndex + 1) : PlayerSettings::_1_64;
+
+	if (newType != settings_->getRecordQuantizationType()) {
+		settings_->setRecordQuantizationType(newType);
+		reflectQuantizationSettings();
+	}
 
 	// Update playing instruments settings
 	for (unsigned char i = 0; i < 6; i++) {
@@ -79,19 +78,6 @@ void SettingsAndFunctionsView::update() {
 			settings_->setDrumInstrumentEventType(i, newType);
 		}
 	}
-
-	/*for (unsigned char i = 0; i < 16; i++) {
-		if (hw_->getButtonState(i) == IButtonHW::DOWN) {
-			unsigned char currentPan;
-			panButtons_->getSelectedButton(currentPan);
-			unsigned char newPattern = currentPan * 16 + i;
-			if (newPattern != currentPattern_) {
-				hw_->setLED(currentPattern_ % 16, ILEDHW::OFF);
-				currentPattern_ = newPattern;
-				hw_->setLED(currentPattern_ % 16, ILEDHW::ON);
-			}
-		}
-	}*/
 }
 
 
