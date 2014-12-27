2d6b5fb13403f5b52d8dc56e81ad8c41ed5dac31
Václav Peloušek <info@bastl-instruments.com>
2014-12-27 13:35:38 +0100

    added defines for functions

--- SettingsAndFunctionsView.cpp
+++ SettingsAndFunctionsView.cpp
@@ -8,6 +8,38 @@
 #include "SettingsAndFunctionsView.h"
 #include <BitArrayOperations.h>
 
+#define MASTER_SLAVE //PLAY_BUTTON
+
+#define QUANTISATION_SUBSTEPS_1 //pan button 1
+#define QUANTISATION_SUBSTEPS_2 //pan button 2
+#define QUANTISATION_SUBSTEPS_4 //pan button 3
+#define QUANTISATION_SUBSTEPS_8 //pan button 4
+
+//the numbers represent step number
+
+#define CLOCK_INPUT_MULTIPLIER_4 1 // being set in the setup at the moment
+#define CLOCK_INPUT_MULTIPLIER_8 2
+#define CLOCK_INPUT_MULTIPLIER_16 3
+#define CLOCK_INPUT_MULTIPLIER_32 4
+
+//here instead of 5 tempo jumps might come something else when we have idea what else it could be
+#define TEMPO_DOWN_5 5 	//jump by 5 BPM
+#define TEMPO_DOWN 6 	//jump by 1 BPM
+#define TEMPO_UP 7 		//jump by 1 BPM
+#define TEMPO_UP_5 8 	//jump by 5 BPM
+
+#define COPY 9				//remembers current pattern, instrument and pan
+#define PASTE 10			// checks last action (selection of pattern, instrument or pan) and copies to the current one
+#define SAVE 11				// saves to card and remembers  adresses of patterns
+#define DISCARD_CHANGES 12	// not ready yet - but changes adresses of patterns
+
+#define CLEAR_PATTERN 13 //put everything to default including actives
+#define CLEAR_INSTRUMENT 14 // delete just steps
+#define CLEAR_ACTIVES_FOR_INSTRUMENT 15 // delete just steps
+#define CLEAR_ACTIVES_FOR_ALL_INSTRUMENTS 16//all actives to default state
+
+
+
 SettingsAndFunctionsView::SettingsAndFunctionsView() : hw_(0),
 								 settings_(0),
 								 instrumentBar_(0),
